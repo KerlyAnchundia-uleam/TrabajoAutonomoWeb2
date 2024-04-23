ee193f963837d437ff0648c6c64f370636f7d4f7
ANCHUNDIA DELGADO KERLY NICOLE <e1314863968@live.uleam.edu.ec>
2024-04-23 14:37:23 -0500

    Agregado Modelo Proveedor y su respectivo CRUD

--- GestionarCompras/obj/Debug/net8.0/GestionarCompras.AssemblyInfo.cs
+++ GestionarCompras/obj/Debug/net8.0/GestionarCompras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionarCompras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d88b3ab7364083c630286486a3e791b00fcc6e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionarCompras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionarCompras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
