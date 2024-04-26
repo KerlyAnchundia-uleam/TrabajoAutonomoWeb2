2d55bf274c2cbbf4268f242fc44e44377f55f14f
ANCHUNDIA DELGADO KERLY NICOLE <e1314863968@live.uleam.edu.ec>
2024-04-25 20:10:53 -0500

    Agregado Modelo Categoria y su respectivo CRUD

---
+++ GestionarCompras/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace GestionarCompras.Models
+{
+    namespace GestionarCompras.Models
+    {
+        public class Categoria
+        {
+            public int IdCategoria { get; set; }
+            public string Nombre { get; set; }
+            public string Descripcion { get; set; }
+        }
+    }
+
+}
